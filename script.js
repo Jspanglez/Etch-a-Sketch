52d1937bdedf225c8868f06dda40a6f31525c983
Jeev Pangli <jspanglez@gmail.com>
2024-07-19 18:07:07 +0100

    Added an event listener for the divs

--- script.js
+++ script.js
@@ -5,8 +5,13 @@ let counter = 1
 do {
   const square = document.createElement("div")
   square.className = "square"
-  /* square.textContent = `${counter}`; */
   container.appendChild(square)
   counter++
 } while (counter <= 256)
 
+const squares = document.querySelectorAll(".square")
+squares.forEach((square) => {
+  square.addEventListener("click", function() {
+    this.classList.toggle("selected")
+  })
+})
