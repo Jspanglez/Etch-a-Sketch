7accce4298e3b174d01d266ce1cc2799ceac20dd
Jeev Pangli <jspanglez@gmail.com>
2024-07-24 15:53:42 +0100

    Added a function that makes the grid

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 const container = document.getElementById("container")
+const btn = document.querySelector('button');
 
-let counter = 1
-
-do {
-  const square = document.createElement("div")
-  square.className = "square"
-  container.appendChild(square)
-  counter++
-} while (counter <= 256)
-
-const squares = document.querySelectorAll(".square")
-squares.forEach((square) => {
-  square.addEventListener("click", function() {
-    this.classList.toggle("selected")
-  })
+function createGrid(input) {
+  for (let i = 0; i < (input * input); i++) {
+    const square = document.createElement('div')
+    const boxSize = 600 / input
+    square.classList.add('square')
+    // square.textContent = i + 1
+    square.style.width = (boxSize - 1.6) + 'px'
+    square.style.height = boxSize + 'px'
+    square.addEventListener("click", function() {
+      this.classList.toggle("selected")
+    })
+    container.appendChild(square)
+  }
+}
 })
